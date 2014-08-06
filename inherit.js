c51e9e472c77b9a3daf2e7a1ec701543518f0d3e
mparke <mparke78@gmail.com>
2014-08-05 21:47:17 -0400

    WIP

--- inherit.js
+++ inherit.js
@@ -1,9 +1,20 @@
-module.exports = function (Parent, Child) {
-  function Proxy () {
-    this.constructor = Child.constructor;
-  };
+(function (window) {
+  
+  function inherit () {
+    function Proxy () {
+      this.constructor = Child.constructor;
+    };
 
-  Proxy.prototype = Parent.prototype;
-  Child.prototype = new Proxy();
-  return Child;
-};
+    Proxy.prototype = Parent.prototype;
+    Child.prototype = new Proxy();
+    return Child;
+  }
+
+  if (typeof module === 'object') {
+    define(function () { return inherit; });
+  } else if (typeof define === 'function') {
+    module.exports = inherit;
+  } else {
+    window.inherit = inherit;
+  }
+})(window);
